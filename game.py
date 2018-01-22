38952f344f565fa4fa4c21b658d3a745215a5d6a
Sarah <hayes.sc@gmail.com>
2018-01-22 06:05:49 -0500

    Adding more functionality to game

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 # draw grid
 # pick random location for player
@@ -16,31 +17,48 @@ CELLS = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
          (0, 3), (1, 3), (2, 3), (3, 3), (4, 3),
          (0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]
 
-def get_locations():
-    monster = None
-    door = None
-    player = None
 
-    return monster, door, player
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def get_locations():
+    return random.sample(CELLS, 3)
 
 def move_player(player, move):
-    # get player's location
-    # if move == LEFT, x-1
-    # if move == RIGHT, x+1
-    # if move == UP, y-1
-    #if move == DOWN, y+1
-    return player
+    x, y = player
+    if move == "LEFT":
+        x -= 1
+    if move == "RIGHT":
+        x += 1
+    if move == "UP":
+        y -= 1
+    if move == "DOWN":
+        y += 1   
+    return x, y
 
 def get_moves(player):
     moves = ["LEFT", "RIGHT", "UP", "DOWN"]
+    x, y = player
+    if x == 0:
+        moves.remove("LEFT")
+    if x == 4:
+        moves.remove("RIGHT")
+    if y == 0:
+        moves.remove("UP")
+    if y == 4:
+        moves.remove("DOWN")        
     return moves
 
-
+monster, door, player = get_locations()
 
 while True:
+    valid_moves = get_moves(player)
+    clear_screen()
     print("Welcome to the dungeon.")
-    print("You are currently in room {}") #fill with player position
-    print("You can move {}") #fill with available moves
+    print("You are currently in room {}".format(player)) #fill with player position
+    print("You can move {}".format(", ".join(valid_moves))) #fill with available moves
     print("Enter QUIT to quit.")
 
     move = input("> ")
@@ -48,6 +66,11 @@ while True:
 
     if move == 'QUIT':
         break
+    if move in valid_moves:
+        player = move_player(player, move)
+    else:
+        print("\n ** Walls are hard. Don't run into them! ** \n")
+        continue
 
     # Good move? Change player position
     # Bad move? Don't change anything
